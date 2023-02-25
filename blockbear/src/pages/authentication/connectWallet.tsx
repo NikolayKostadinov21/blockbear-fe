09ba81e19b726370201ed27e8c6eceb80949a180
Nikolay Kostadinov <nikolay.kostadinov@pliant.io>
2023-02-25 20:28:26 +0200

    Successfully connecting the client with metamask

--- blockbear/src/pages/authentication/connectWallet.tsx
+++ blockbear/src/pages/authentication/connectWallet.tsx
@@ -1,12 +1,41 @@
 import { Button } from "@mui/material";
+import { ethers } from "ethers";
+import { useState } from "react";
 import "../authentication/connectWallet.scss";
 
 const ConnectWallet = () => {
+
+    const [isConnected, setIsConnected] = useState(false);
+    const [hasMetaMask, setHasMetaMask] = useState(false);
+    const [signer, setSigner] = useState(undefined);
+    const [currentAccount, setCurrentAccount] = useState();
+
+    async function connect() {
+        if (typeof window.ethereum !== "undefined") {
+            try {
+                const { ethereum } = window;
+                console.log(window.ethereum);
+                setIsConnected(true);
+                const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+                setSigner(provider.getSigner() as any);
+                console.log(provider.getSigner()._address);
+                if (ethereum) {
+                    const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+                    accounts ? setCurrentAccount(accounts as any) : console.log('error');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            setIsConnected(false);
+        }
+    }
+
     return (
         <div>
             <div className="connect-to-wallet-container">
                 <div className="connect-to-wallet-button">
-                    <Button variant="contained">Connect with MetaMask</Button>
+                    <Button variant="contained" onClick={connect}>Connect with MetaMask</Button>
                 </div>
             </div>
         </div>
