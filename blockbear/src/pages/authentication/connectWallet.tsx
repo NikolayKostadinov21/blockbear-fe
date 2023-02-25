4a65f9b471ce34a7f05a3013ab259c53f3124b67
Nikolay Kostadinov <nikolay.kostadinov@pliant.io>
2023-02-25 14:30:04 +0200

    Installing react material with sass and adding custom button for test purposes

--- blockbear/src/pages/authentication/connectWallet.tsx
+++ blockbear/src/pages/authentication/connectWallet.tsx
@@ -1,6 +1,15 @@
+import { Button } from "@mui/material";
+import "../authentication/connectWallet.scss";
+
 const ConnectWallet = () => {
-    return(
-        <div>test</div>
+    return (
+        <div>
+            <div className="connect-to-wallet-container">
+                <div className="connect-to-wallet-button">
+                    <Button variant="contained">Connect with MetaMask</Button>
+                </div>
+            </div>
+        </div>
     );
 }
 
